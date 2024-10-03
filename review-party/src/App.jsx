c7209fed9db97983adc8df7f608f83809967909e
Alexis Sushko <alexis.sushko@gmail.com>
2024-10-03 13:34:32 -0400

    Created api url, components folder with partyform, partylist, and selected party

--- review-party/src/App.jsx
+++ review-party/src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import { useState } from "react";
+import store from "./store/store";
 
 function App() {
   return <></>;
---
+++ review-party/src/store/api.js
@@ -0,0 +1,12 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+const COHORT = "2408-Alexis";
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api${COHORT}/`;
+
+const api = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: API_URL,
+  }),
+  endpoints: () => ({}),
+  tagTypes: ["Party"],
+});
+export default api;
